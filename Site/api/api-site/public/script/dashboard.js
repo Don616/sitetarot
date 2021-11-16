bffe07f84a595da75b93a62a74c2b329ac5dbe9b
Don616 <contatodonilo@gmail.com>
2021-11-16 00:26:40 -0300

    comecei a dash

---
+++ Site/api/api-site/public/script/dashboard.js
@@ -0,0 +1,23 @@
+
+
+function grafico1(){
+
+    conteudo1.style.display = 'flex';
+    conteudo2.style.display = 'none';
+    conteudo3.style.display = 'none';
+
+}
+function grafico2(){
+
+    conteudo1.style.display = 'none';
+    conteudo2.style.display = 'flex';
+    conteudo3.style.display = 'none';
+
+}
+function grafico3(){
+
+    conteudo1.style.display = 'none';
+    conteudo2.style.display = 'none';
+    conteudo3.style.display = 'flex';
+
+}
