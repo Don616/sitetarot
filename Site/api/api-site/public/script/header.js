6cfe35e60c0507fc4a8874b3ff40c7f949553862
Don616 <contatodonilo@gmail.com>
2021-11-26 09:28:35 -0300

    mudei o header

--- Site/api/api-site/public/script/header.js
+++ Site/api/api-site/public/script/header.js
@@ -3,21 +3,24 @@ cabeça()
 function cabeça(){
 
     var id_user = localStorage.getItem('ID_USUARIO');
-    console.log(id_user)
+    
+    
+    //console.log(id_user)
     if(id_user != undefined){
-        console.log(id_user)
+        //console.log(id_user)
         header.style.display = `none`
         header2.style.display = `flex`
     } else {
         header.style.display = `flex`
         header2.style.display = `none`
 }
+    //nome_logado.innerHTML = `Olá, ${nome_user}`
 
 }
 
 function deslogar(){
-
-    var sair = confirm('Tem certeza que deseja sair?',);
+    var nome_user = localStorage.getItem('NOME_USUARIO');
+    var sair = confirm(`Tem certeza que deseja sair, ${nome_user}?`,);
     if(sair == true){
 
         localStorage.clear();
